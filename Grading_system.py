dc39d0aa19474b1cb845b4986c5d9601d62289b6
Toshashusingh <110317529+Toshashusingh@users.noreply.github.com>
2023-03-30 13:45:00 +0530

    Grade Calculator
    -Grade is calculated based on the score b/w 0 to 1
    -any other no, Error is displayed saying ,enter no b/w 0 to 1

---
+++ Grading_system.py
@@ -0,0 +1,21 @@
+score=input("Enter Score:")
+s=float(score)
+if s>1:
+    print('Enter no between 0 to 1')
+elif s<0:
+    print('Enter no between 0 to 1')
+elif s>= 0.9:
+    print('A')
+elif s>= 0.8:
+    print('B')
+elif s>= 0.7:
+    print('C')
+elif s>= 0.6:
+    print('D')
+elif s<.6:
+    print("F")
+
+else:
+    print("A")
+    
+     
